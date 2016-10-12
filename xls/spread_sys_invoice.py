759c34c891162f5a6cd49af2de8dd86bf0dfee1f
Change586 <ccxsjjs242@126.com>
2016-10-12 17:15:12 +0800

    test

--- xls/spread_sys_invoice.py
+++ xls/spread_sys_invoice.py
@@ -4,15 +4,6 @@
 from read_sheet import get_data
 from collections import Counter
 
-# path = r'D:\processed_data.xls'
-# read_table_index = 1
-# processed_col = 24
-#
-# data = get_data()
-#
-# table_data = data.get_table_data(path,read_table_index)
-# col_values = data.get_col_values(table_data,processed_col-1)
-
 def spread_sys_invoice(col_value):
     data = get_data()
     sys_invoice_num = data.shifting_extract_col_value(col_value,'发票号',4,12)
@@ -30,8 +21,7 @@ def spread_sys_invoice(col_value):
         sys_invoices_temp_list = extract_col_value.split('/')
         # print sys_invoices_temp_list
 
-        up_leng = None
-        up_sys_invoice = None
+        last_sys_invoice = sys_invoice_num
 
         for sys_invoice in sys_invoices_temp_list:
             if '-' in sys_invoice:
@@ -41,101 +31,48 @@ def spread_sys_invoice(col_value):
 
                 if leng != sys_invoice_len:
                     #处理 60364208-4233/4268-4337/49/57-69和60363381-83/3463-3477/3535-38/3587-3661/65-77/3752-61/64-69/86两种特殊情况
-                    if up_leng:
-                        if up_leng > leng: #4268-4337/49/57-69
-                            if leng == (len_sys_invoice-1)/2:
-                                end_index = sys_invoice_len -up_leng
-                                end_index2 = leng + up_leng - leng
-                                prefix = sys_invoice_num[:end_index] + up_sys_invoice[leng+1:end_index2]
-                                up_sys_invoice = sys_invoice #只能放在这个位置，前后都用到了该变量
-                                up_leng = leng
-                                sys_invoice = prefix + sys_invoice[:leng+1] + prefix + sys_invoice[leng+1:]
-                                sys_invoice_list.append(sys_invoice)
-                            else:
-                                end_index = sys_invoice_len - up_leng
-                                end_index2 = up_leng - leng
-                                prefix = sys_invoice_num[:end_index] + up_sys_invoice[:end_index2]
-                                up_sys_invoice = sys_invoice
-                                sys_invoice = prefix + sys_invoice[:leng + 1] + prefix + sys_invoice[leng + 1:]
-                                sys_invoice_list.append(sys_invoice)
-                                up_leng = leng
-                        else: # 65-77/3752-61情况
-                            if leng == (len_sys_invoice - 1) / 2:
-                                prefix = sys_invoice_num[:end_index]
-                                up_sys_invoice = sys_invoice
-                                sys_invoice = prefix + sys_invoice[:leng + 1] + prefix + sys_invoice[leng + 1:]
-                                sys_invoice_list.append(sys_invoice)
-                                up_leng = leng
-                            else:
-                                prefix = sys_invoice_num[:end_index]
-                                space_len = 2*leng-len_sys_invoice
-                                prefix2 = prefix + sys_invoice[:space_len]
-                                up_sys_invoice = sys_invoice
-                                sys_invoice = prefix + sys_invoice[:leng + 1] + prefix2 + sys_invoice[leng + 1:]
-                                sys_invoice_list.append(sys_invoice)
-                                up_leng = leng
-                    else:
-                        prefix = sys_invoice_num[:end_index]
-                        up_sys_invoice = sys_invoice
-                        sys_invoice = prefix + sys_invoice[:leng + 1] + prefix + sys_invoice[leng + 1:]
+                    if leng == len_sys_invoice/2:
+                        prefix = last_sys_invoice[:end_index]
+                        last_sys_invoice = prefix + sys_invoice[leng+1:]
+                        sys_invoice = prefix + sys_invoice[:leng+1] + last_sys_invoice
                         sys_invoice_list.append(sys_invoice)
-                        up_leng = leng
-                else:
-                    leng2 = len(sys_invoice[leng+1:])
-                    end_index = sys_invoice_len - leng2
-                    prefix = sys_invoice_num[:end_index]
-                    sys_invoice = sys_invoice[:leng+1]+prefix+sys_invoice[leng+1:]
-                    sys_invoice_list.append(sys_invoice)
-                    sys_invoice_num = sys_invoice[:leng]
-                    up_leng = None
-                    up_sys_invoice = None
-            else:
+                    else:
+                        leng2 = len_sys_invoice-leng-1
+                        prefix = last_sys_invoice[:end_index]
+                        prefix2 = last_sys_invoice[:end_index]+sys_invoice[:leng-leng2]
+                        last_sys_invoice = prefix2 + sys_invoice[leng+1:]
+                        sys_invoice = prefix + sys_invoice[:leng+1] + last_sys_invoice
+                        sys_invoice_list.append(sys_invoice)
+                else: #60364208-4233/4268-4337/60363381-83
+                    if leng == len_sys_invoice/2:
+                        last_sys_invoice = sys_invoice[leng+1:]
+                        sys_invoice = sys_invoice[:leng+1] + last_sys_invoice
+                        sys_invoice_list.append(sys_invoice)
+                    else:
+                        leng2 = len_sys_invoice-leng-1
+                        prefix = sys_invoice[:leng-leng2]
+                        last_sys_invoice = prefix + sys_invoice[leng+1:]
+                        sys_invoice = sys_invoice[:leng+1] + last_sys_invoice
+                        sys_invoice_list.append(sys_invoice)
+            else: #没有‘-’
                 leng = len(sys_invoice)
                 end_index = sys_invoice_len - leng
 
                 if leng != sys_invoice_len:
-                    if up_leng:
-                        if up_leng > leng:
-                            if leng == (len_sys_invoice-1)/2:
-                                end_index = sys_invoice_len -up_leng
-                                end_index2 = leng + up_leng - leng
-                                prefix = sys_invoice_num[:end_index] + up_sys_invoice[leng+1:end_index2]
-                                up_sys_invoice = sys_invoice
-                                sys_invoice = prefix + sys_invoice[:leng]
-                                sys_invoice_list.append(sys_invoice)
-                                up_leng = leng
-                            else:
-                                end_index = sys_invoice_len - up_leng
-                                end_index2 = up_leng - leng
-                                prefix = sys_invoice_num[:end_index] + up_sys_invoice[:end_index2]
-                                up_sys_invoice = sys_invoice
-                                sys_invoice = prefix + sys_invoice[:leng]
-                                sys_invoice_list.append(sys_invoice)
-                                up_leng = leng
-                        else:
-                            prefix = sys_invoice_num[:end_index]
-                            up_sys_invoice = sys_invoice
-                            sys_invoice = prefix + sys_invoice[:leng]
-                            sys_invoice_list.append(sys_invoice)
-                            up_leng = leng
-                    else:
-                        prefix = sys_invoice_num[:end_index]
-                        up_sys_invoice = sys_invoice
-                        sys_invoice = prefix + sys_invoice[:leng]
-                        sys_invoice_list.append(sys_invoice)
-                        up_leng = leng
-                else:
+                    prefix = last_sys_invoice[:end_index]
+                    last_sys_invoice = prefix + sys_invoice
+                    sys_invoice = last_sys_invoice
                     sys_invoice_list.append(sys_invoice)
-                    sys_invoice_num = sys_invoice
-                    up_leng = None
-                    up_sys_invoice = None
-            print up_leng,up_sys_invoice
+                else:
+                    last_sys_invoice = sys_invoice
+                    sys_invoice_list.append(sys_invoice)
+            # print up_leng,up_sys_invoice
 
 
     else:
         sys_invoice_list.append(sys_invoice_num)
 
-    print sys_invoice_list
+    # print sys_invoice_list
 
     #展开所有的系统发票号
     spread_sys_invoice_list = []
@@ -165,6 +102,7 @@ if __name__ == '__main__':
         spread_sys_invoice_list = spread_sys_invoice(sys_invoice_string)
 
         #print spread_sys_invoice_list
+
 
 
 
