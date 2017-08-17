a0b762e853e6be2d5b75d9f6fd339dda4f472aa7
change <ccxsjjs242@126.com>
2017-08-17 23:20:16 +0800

    te

--- InterfaceTest/interface_test_automation/test.py
+++ InterfaceTest/interface_test_automation/test.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import urllib2
-import urllib
+# import os
+#
+# def getfilename(filepath):
+#     for root,dirs,files in os.walk(filepath):
+#
+#         for filename in files:
+#             yield filename
+#
+#
+#
+# path = "H:\\"
+#
+# # getfilename(path)
+# for file in getfilename(path):
+#     print(file)
 
-url = 'http://www.baidu.com'
+def count(n):
+  for i in range(4):
+    print("cunting: "+str(i))
+    yield n  #生成值：n
+    n -= 1
 
-
-
-from urllib import request
+a=count(5)
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
+print(a.__next__())
